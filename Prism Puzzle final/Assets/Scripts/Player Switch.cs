3acf88ec2c004ca15dbf3871b3335770801de976
lxrdflame <tyrique.s.r@gmail.com>
2024-05-20 16:41:38 +0200

    Player Switch Mechanic
    i just added the player switching mechanic and what changed the sizes and positions of the boards

---
+++ Prism Puzzle final/Assets/Scripts/Player Switch.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwitch : MonoBehaviour
+{
+    private GameObject PlayerSwapPanel;
+    private Vector3 StartPosition = new Vector3(11.27f, 0, 90);
+    private Vector3 NextPosition = new Vector3(-11.27f, 0, 90);
+
+    private GameObject Player1PuzzleBoard;
+    private GameObject Player2PuzzleBoard;
+
+    private Vector3 SmallBoardSize = new Vector3(0.5f , 0.5f, 0);
+
+    public Vector3 SmallBlockPosition = new Vector3(-0.186f, 3.492f, 90);
+    public Vector3 OriginalPosition = new Vector3(2.768f, -1.786f, 90);
+
+
+    public Vector3 SmallBlock2Position = new Vector3(-0.186f, 3.492f, 90);
+    public Vector3 Original2Position = new Vector3(2.768f, -1.786f, 90);
+
+    private Vector3 OriginalSize = new Vector3(1,1,0);
+
+
+
+    public void Start()
+    {
+        PlayerSwapPanel = GameObject.FindGameObjectWithTag("DragPanel");
+        
+
+        Player1PuzzleBoard = GameObject.FindGameObjectWithTag("Player1Board");
+        Player2PuzzleBoard = GameObject.FindGameObjectWithTag("Player2Board");
+
+    }
+
+
+    public void SwitchTOPLayer1()
+    {
+        PlayerSwapPanel.transform.position = StartPosition;
+        Player2PuzzleBoard.transform.localScale = SmallBoardSize;
+        Player2PuzzleBoard.transform.position = SmallBlock2Position;
+
+        Player1PuzzleBoard.transform.position = OriginalPosition;
+        Player1PuzzleBoard.transform.localScale = OriginalSize;
+        
+
+    }
+
+    public void SwitchTOPLayer2()
+    {
+        PlayerSwapPanel.transform.position = NextPosition;
+        Player1PuzzleBoard.transform.localScale = SmallBoardSize;
+        Player1PuzzleBoard.transform.position = SmallBlockPosition;
+
+        Player2PuzzleBoard.transform.localScale = OriginalSize;
+        Player2PuzzleBoard.transform.position = Original2Position;
+
+    }
+}
