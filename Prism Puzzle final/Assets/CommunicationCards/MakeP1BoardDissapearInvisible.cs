7be5cb4ea8a6451a5ebfbe5742f9122ade8ec960
ttchabs <tchabs334@gmail.com>
2024-05-22 13:10:26 +0200

    Works like 80%
    Boards disspear when card panles show. should be fine.

---
+++ Prism Puzzle final/Assets/CommunicationCards/MakeP1BoardDissapearInvisible.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeP1BoardDissapearInvisible : MonoBehaviour
+{
+   
+}
